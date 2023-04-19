18a542bd991ee71903a26fca0e02ac68a09a0d4e
Ettore Muniz <ettore.barbosa@gmail.com>
2023-04-19 08:07:33 -0300

    fix: head2

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head></Head>
+      <Head title="chapeira digital" />
 
       <body>
         <div id="loader-root" />
