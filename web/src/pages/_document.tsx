924c006af7ccc3c11d1ac2f7a3bc47ca1aadb62e
Ettore Muniz <ettore.barbosa@gmail.com>
2023-04-19 08:17:06 -0300

    heas

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head title="chapeira digital" />
+      <Head title="chapeira digital"></Head>
 
       <body>
         <div id="loader-root" />
